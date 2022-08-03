1ee763a688355b035560c985b7e42831d0dbf0bc
Hesam Mousavi <h.mousavi9877@gmail.com>
2022-08-03 15:44:55 +0430

    Aug 3, 15:45: sympone lecture 1

--- learning/php/Sympony/hello-world/front.php
+++ learning/php/Sympony/hello-world/front.php
@@ -4,16 +4,17 @@ require_once dirname(__DIR__).'/src/app.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 $response = new Response();
 
 dump($request->getPathInfo()); // get url pat
 
-$map = [
-    '/hello' => dirname(__DIR__).'/src/pages/hello.php',
-    '/bye' => dirname(__DIR__).'/src/pages/bye.php',
-];
+// $map = [
+//     '/hello' => dirname(__DIR__).'/src/pages/hello.php',
+//     '/bye' => dirname(__DIR__).'/src/pages/bye.php',
+// ];
 
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
@@ -22,12 +23,25 @@ $requestContext = new RequestContext();
 $requestContext->fromRequest($request);
 $matcher = new UrlMatcher($routes, $request);
 
-// dump($matcher->match($request->getPathInfo())); 
 
-if(isset($map[$request->getPathInfo()])) {
-    $response = require $map[$request->getPathInfo()];
-} else {
-    $response = new Response('Not Found', 404);
-} 
+// Can do try catch globally
+
+try {
+    // dump($matcher->match($request->getPathInfo())); 
+    $attributes = dump($matcher->match($request->getPathInfo())); 
+
+    extract($attributes);
+    require sprintf(dirname(__DIR__).'/src/pages/'.'/src/pages/'.$attributes['_route'].'.php', $attributes);
+} catch (ResourceNotFoundException $e) {
+    $response->setContent('Page not found', 404);
+} catch (Exception $e) {
+    $response->setContent('Internal error', 500);
+}
+
+// if(isset($map[$request->getPathInfo()])) {
+//     $response = require $map[$request->getPathInfo()];
+// } else {
+//     $response = new Response('Not Found', 404);
+// } 
 
 $response->prepare($request)->send();
